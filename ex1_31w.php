c749aaa8e586180f9460405ff8a14cb4f4cf6022
Dev2023 <e2194608@cmaisonneuve.qc.ca>
2022-06-06 12:47:13 -0400

    s3-c2 Config du premier plugin

--- ex1_31w.php
+++ ex1_31w.php
@@ -1,6 +1,19 @@
 <?php 
 /**
- * plugin name: Extensio de l'exercice 1
- * Description : premier extension pour l'exercice 1 
- * auteur URL : https://github.com/Dev2023/ 
+ * Plugin Name: Extension de l'exercice 1
+ * Author: Yordan Toledano Prieto 
+ * Description: Premier extension pour l'exercice 1 
+ * Author URI: https://github.com/Dev2023/31w-extension1 
  */
+?>
+<?php
+    function function_general() {
+        function mon_31w_enqueue() {
+            add_theme_support( 'title-tag' );
+            add_theme_support( 'html5', array('search-form') );
+            add_theme_support('custom-background');
+        }
+        add_action( 'after_setup_theme', 'mon_31w_setup' );
+    }
+    add_action('init', 'enqueue_style') //commentaire
+?>
